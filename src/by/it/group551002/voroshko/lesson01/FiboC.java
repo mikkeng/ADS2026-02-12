cd8fb2599da2843dd80224a558267223779adebc
Mikhail Varoshka <mishavoroshko2007@gamil.com>
2026-02-19 08:11:02 +0300

    completed FiboA, FiboB, FiboC

--- src/by/it/group551002/voroshko/lesson01/FiboC.java
+++ src/by/it/group551002/voroshko/lesson01/FiboC.java
@@ -37,7 +37,7 @@ public class FiboC {
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 for (int k = 0; k < 2; k++) {
-                    long res = (arrA[i][k] % m) + (arrB[k][j] % m);
+                    long res = (arrA[i][k] % m) * (arrB[k][j] % m);
                     arrC[i][j] = (arrC[i][j] + res) % m;
                 }
             }
